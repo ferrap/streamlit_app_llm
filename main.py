8302a3629fcaaf91d9e88a9f7045a39e9f12dd7c
Paolo Ferraiuoli <48998427+ferrap@users.noreply.github.com>
2023-05-16 13:56:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ llm = OpenAI(temperature=0)
 @st.experimental_singleton
 def build_bq_chain():
     engine = create_engine(
-        'bigquery://silent-vim-249116.wqe/bigquery-public-data', credentials_info=st.secrets['gcp_service_account']
+        'bigquery://silent-vim-249116/bigquery-public-data', credentials_info=st.secrets['gcp_service_account']
         
     )
 
